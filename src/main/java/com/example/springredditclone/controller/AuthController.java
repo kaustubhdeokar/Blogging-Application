4e9b8b7f5c2016d56080b4b9410b2a9b21218dae
kaustubhdeokar <kaustubhd9@gmail.com>
2022-05-27 16:17:08 +0530

    refresh token created, deleted with api calls

--- src/main/java/com/example/springredditclone/controller/AuthController.java
+++ src/main/java/com/example/springredditclone/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.example.springredditclone.controller;
 
 import com.example.springredditclone.dto.AuthenticationResponse;
 import com.example.springredditclone.dto.LoginRequest;
+import com.example.springredditclone.dto.RefreshTokenRequest;
 import com.example.springredditclone.dto.RegisterRequest;
 import com.example.springredditclone.service.AuthService;
+import com.example.springredditclone.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
+
 @Controller
 
 @AllArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/api/auth/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
@@ -32,7 +37,7 @@ public class AuthController {
     public  ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         //we should check if the account is verified or not.
         AuthenticationResponse login = authService.login(loginRequest);
-        return new ResponseEntity<>(login.getAuthenticationToken(),HttpStatus.OK);
+        return new ResponseEntity<>(login.toString(),HttpStatus.OK);
     }
 
     @GetMapping("/api/auth/accountVerification/{token}")
@@ -40,5 +45,18 @@ public class AuthController {
         authService.verifyAccount(token);
         return new ResponseEntity<>("Account verified!", HttpStatus.OK);
     }
+
+    @PostMapping("/api/auth/refresh/token")
+    public ResponseEntity<String> refreshToken(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest){
+        AuthenticationResponse authenticationResponse = authService.refreshToken(refreshTokenRequest);
+        return new ResponseEntity<>(authenticationResponse.toString(), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/auth/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest){
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return new ResponseEntity<>("Token deleted successfully.", HttpStatus.OK);
+    }
+
 
 }

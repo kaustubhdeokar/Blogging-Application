78c38fa7f554e899d0c509fda09e5951783b8d34
kaustubhdeokar <kaustubhd9@gmail.com>
2024-03-16 03:46:40 +0530

    node modules removed

--- src/main/java/com/example/reddit/repo/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.reddit.repo;
-
-import com.example.reddit.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
